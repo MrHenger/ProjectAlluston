28dc4665f31de25a1c6a20f2fa7b2cbf352d0a0b
MrHenger <henry.r0701@gmail.com>
2021-03-01 14:45:58 -0400

    configuracion de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\AdminUserController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Video;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +22,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/prueba', function () {
+    if (!$video = Video::where('id', 2)->first()) {
+        echo 'encontrado';
+        echo var_dump($video);
+    }
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/post/list', [PostController::class, 'showList'])->name('post.list');
+
+Route::resource('/post', PostController::class);
+
+Route::resource('/user', AdminUserController::class);
