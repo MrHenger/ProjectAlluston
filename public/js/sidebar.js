bad55bcfe4b25d0e301c90416b46e28f3751762c
MrHenger <henry.r0701@gmail.com>
2021-03-05 13:55:09 -0400

    Nueva funcion del sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,17 +1,21 @@
-'use strict'
+"use strict";
 
-
-var check = document.querySelector('#check');
-var sidebar = document.querySelector('#nav-sidebar');
-
+var check = document.querySelector("#check");
+var sidebar = document.querySelector("#nav-sidebar");
+var arrowBack = document.querySelector(".arrow-back");
 
 check.addEventListener("change", validaCheckbox, false);
-function validaCheckbox()
-{
-  
-    if(check.checked){
-        sidebar.classList.toggle('show-sidebar');
-    }else if(!check.checked){
-        sidebar.classList.toggle('show-sidebar');
-    }  
+/* arrowBack.addEventListener("click", validaCheckbox, false); */
+
+$(".arrow-back").click(function () {
+    //Esto es hace lo mismo que lo de arriba pero con JQuery
+    validaCheckbox();
+});
+
+function validaCheckbox() {
+    if (check.checked) {
+        sidebar.classList.toggle("show-sidebar");
+    } else if (!check.checked) {
+        sidebar.classList.toggle("show-sidebar");
+    }
 }
