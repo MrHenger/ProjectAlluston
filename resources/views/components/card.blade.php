b66b1a09e81e3dcd7522aeffb938d9758b1add76
MrHenger <henry.r0701@gmail.com>
2021-03-05 13:48:55 -0400

    Implementacion del componente card

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,12 @@
+
+<div class="card shadow mb-5 ">
+    <img src="{{asset('/images/miniatures/'.$image)}}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">{{$title}}</h5>
+        {{-- <p class="card-text">{{$slot}}</p> --}}
+        <div class="text-center">
+        <a href="#" class="btn btn-primary">Mas Informacion</a>
+        </div>
+        
+    </div>
+</div>
